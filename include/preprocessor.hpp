f85371e7381d41c791f0e071ece38a79a7b74a29
Ananth Junutula <ananthkrj@gmail.com>
2025-07-07 02:28:55 -0700

    preprocess structure

--- include/preprocessor.hpp
+++ include/preprocessor.hpp
@@ -1,41 +1,57 @@
 #pragma once
 #include <opencv2/opencv.hpp>
-#include <vector>
 
-// Create a class that will act as a wrapper for preprocessing
-// the image input
+/* The goal of preprocessing is to preprocess the input,
+resize, convert Bgrt to rgb, normalize, and convertoblob
+*/
 
-// preprocessing will need constructor/destructor and member functions
-// that will execute these actions:
-// OpenCV c++: resizing, pad, and normalize image output
+// struct for preprocessinginfo
+// difference in constructor for stuct and oop constructor?
+struct PreprocessInfo {
+    float scale_x;
+    float scale_y;
+    int pad_x;
+    int pad_y;
+    // specifiy size of image
+    cv::Size original_size;
+    cv::Size target_size;
+
+    // constructor for struct, as we use field initialization
+    // in struct
+}
+
 class Preprocessor {
+private:
+    // target dimensions
+    int target_width_;
+    int target_height_;
+
+    // normalization parameters
+    cv::Scalar mean_;
+    cv::Scalar std_;
+
+    // last preprocessing info
+    // from struct
+    PreProcessInfo last_preprocess_info_;
 
 public:
-// constructor
-// dimensions of image
-    preprocessor(int inputWidth, int inputHeight);
-// destructor
-    ~preprocessor();
+    // question:
+    // why does only engine.cpp have a destructor why not
+    // other files? Is raii still being upheld in other files?
+    // compile generated destructor does correcmtly already
 
-// public member functions
+    // constructor
 
-// need to process image, convert image to model input format
-// pass by image by reference
-bool preprocess(const cv:Mat& image, float* output);
+    // main preprocessing pipeline
 
-// need to resize to model dimensions
-// also pass unchangebale image by reference
-cv::Mat resizeImage(const cv::Mat& image);
+    // individual preprocessing steps
 
-// and normalize pixel values to floated memory where
-// neormalized floats will go
-// float* buffer is a pointer to pre-alloca
-void normalizeImage(const cv::Mat& image, float* buffer);
+    // getters
 
+    // setters
+
+// why private again?
 private:
-// parameters to pass into constructor
-    int m_inputWidth;
-    int m_inputHeight;
-
-    // target mean/std for normalization
+    // private helper methods
+    
 };
