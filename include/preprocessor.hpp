fcc419fceb571382cdd35cbe2b8b9162ac8260b1
Ananth Junutula <ananthkrj@gmail.com>
2025-07-07 17:34:43 -0700

    preprocess header

--- include/preprocessor.hpp
+++ include/preprocessor.hpp
@@ -1,3 +1,7 @@
+// header guards
+#ifndef PREPROCESSOR_HPP
+#define PREPROCESSOR_HPP
+
 #pragma once
 #include <opencv2/opencv.hpp>
 
@@ -41,17 +45,43 @@ public:
     // compile generated destructor does correcmtly already
 
     // constructor
+    // parameters will be target width and target height
+    // dimensions should be 640x640
+    Preprocessor(int target_width = 640, int target_height = 640);
 
     // main preprocessing pipeline
+    // cummmulative preprocessing member function
+    // all the preprocessing member functions have cv::Mat
+    // as there data types
+    cv::Mat preprocess(const cv::Mat& input);
 
     // individual preprocessing steps
+    // individual components of preprocessing is resizing
+    // with padding, converting bgrt to rgb, normalizing,
+    // and converting to blob
+    cv::Mat resizeWithPadding(const cv::Mat& input, int target_width, int target_height);
+    cv::Mat convertBGRTtoRGB(const cv::Mat& input);
+    cv::Mat normalize(const cv::Mat& input);
+    cv::Mat convertoBlob(const cv::Mat& input);
 
     // getters
+    // need to get the struct preprocessing info
+    // need to get the target size
+    PreprocessInfo getLastPreprocessInfo() const;
+    cv::Size getTargetSize() const;
 
     // setters
+    void setTargetSize(int width, int height);
+    void setNormalizationParams(const cv::Scalar& mean, condt cv::Scalar& std);
 
 // why private again?
 private:
     // private helper methods
-    
+    // private helper method for updating preprocessinfo
+    void updatePreprocessInfo(const cv::Mat& original, const cv::Mat& processed,
+                              float scale_x, float scale_y, int pad_x, int pad_y);
+
 };
+
+// header guard
+#endif
