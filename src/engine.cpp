f355268b67445968d46d98f34a671a17e89b23b1
Ananth Junutula <ananthkrj@gmail.com>
2025-07-19 11:01:22 -0700

    run inference

--- src/engine.cpp
+++ src/engine.cpp
@@ -24,6 +24,8 @@ Engine::Engine() {
 // the constructor
 
 // Load ONNX model from file path
+
+// what to test: 
 bool Engine::loadModel(const std::string& modelPath) {
     // TODO: Check if model file exists
     // TODO: Create ONNX Runtime environment
@@ -70,7 +72,7 @@ bool Engine::loadModel(const std::string& modelPath) {
         input_names_.push_back(name); 
 
         // utilize type info ort struct, type info -> tensor info -> tensorshapeandinfo
-        // need these series of steps to populate the input shape
+        // need these series of steps to populate the input shape 
         Ort::TypeInfo type_info = session_->GetTypeInfo();
         auto tensor_info = session_->GetTensorTypeAndShapeInfo();
         input_shapes_.push_back(tensor_info.GetShape());
@@ -93,17 +95,40 @@ bool Engine::loadModel(const std::string& modelPath) {
 
 }
 
+// before moving onto other memb functions, write down all onnx methods
+
 // Run inference on preprocessed input
+
+// find out what it means to run inference on preprocessed input
 std::vector<float> Engine::runInference(const cv::Mat& preprocessedInput) {
-    // TODO: Create input tensor from cv::Mat data
-    // TODO: Prepare input tensor array for session
-    // TODO: Prepare output tensor array for session
-    // TODO: Run inference session
-    // TODO: Extract output data from tensor
-    // TODO: Convert output to std::vector<float>
-    // TODO: Return output vector
+    // definition of runningInference: Takes the preprocessedInput and runs it through
+    // the model, and return the results
+
+    // 1. Convert preprocessedInput to a float if needed
+    // find out what begin and end does, how begin and end
+    // allows me to fully convert a cv::Mat object into a float object
+    std::vector<float> inputTensorValues = (preprocessedInput.begin<float>, preprocessedInput.end<float>);
+    // 2. create an onnx tensor from the preprocessedinput (integral)
+
+    // define input tensor shape as {1, 3, 640, 640}
+    std::vector<int64_t> tensorShape = {1, 3, preprocessedInput.rows, preprocessedInput.cols};
+
+    // create tensor, usitlise memoryinfo
+    // tensor data will reside on the CPU, so need to use CreateCPU api part of MemoryInfo
+    // creating tensors using memoryInfo class, and createCPU memberfunction of this class, along with device
+    // allocator and OrtmemTypeCPU as parameters
+    Ort::MemoryType memoryType = Ort::MemoryType::CreateCPU(OrtDeviceAllocator, OrtMemTypeCPU);
+
+    // create variable called input tensor and initialize it with the CreateTensor Ort API function
+    // initialize CreateTensor with data, shape, and data type
+    Ort::Value tensorInfo = Ort::Value::CreateTensor()
+
+    // 4. run the session (integral)
+    // 5. convert Ort::value to a float value
+    // 6. return result to be preprocessed
 }
 
+    
 // Get model input dimensions
 cv::Size Engine::getInputSize() const {
     // TODO: Return model input dimensions (usually 640x640 for YOLOv8)
@@ -126,8 +151,12 @@ bool Engine::initializeSession(const std::string& model_path) {
 
 void Engine::extractModelMetadata() {
     // extract model metadata
+
+    // Get the input/output names and shapes
 }
 
 std::vector<float> Engine::tensorToVector(const Ort::Value& tensor) {
-    // convert tensor to vector
+    // helper function to convert tensor to vector, will be used in runInference
+
+    // Convert Ort::value tensor to a c++ vector
 }
