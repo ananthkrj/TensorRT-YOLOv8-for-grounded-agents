51ae9366b0409479d23d85a6f92a68fb473fc8f5
Ananth Junutula <ananthkrj@gmail.com>
2025-07-20 04:30:36 -0700

    engine.cpp done, need to test

--- src/engine.cpp
+++ src/engine.cpp
@@ -4,14 +4,11 @@
 #include <filesystem>
 
 /*
-Where we load the model and extract its metadata using onnx runtime calls
+Constructor where we intiialize session components such as env 
+and session_options + optimization. Will be used to initialize
+session object in initializeSession() member function
 */
 Engine::Engine() {
-    // initiliaze onnx runtime components
-    // TODO: Initialize the session characteristics
-    // TODO: Initialize input/output names
-    // TODO: Set default model dimensions (640x640 for YOLOv8)
-
     // initialize the environment name
     env_ = std::make_unique<Ort::Env>(ORT_LOGGING_LEVEL_WARNING, "Yolov8Engine");
     // initialize the session options as well
@@ -23,83 +20,49 @@ Engine::Engine() {
 // no need for a destructor, because i am not allocating anything in the
 // the constructor
 
-// Load ONNX model from file path
-
-// what to test: 
+/**
+ * check if initialize sesison works and call
+ * extractModelMetaData member fucntion
+*/
 bool Engine::loadModel(const std::string& modelPath) {
-    // TODO: Check if model file exists
-    // TODO: Create ONNX Runtime environment
-    // TODO: Set session options (optimization level, thread count)
-    // TODO: Create inference session from model file
-    // TODO: Get input/output tensor info (names, shapes, types)
-    // TODO: Store input/output metadata for later use
-    // TODO: Return true if successful, false otherwise
 
-    // check if model file exists
+    // check if initializesession works
+    if (!(initializeSession(modelPath))) {
+        std::cerr << "Session is not initialized";
+    }
+    
+    // and call extractModelMetaDaata
+    extractModelMetaData();
+    return true;
+}
+
+/**
+ * Initializes session
+*/
+bool Engine::initializeSession(const std::string& model_path) {
+    // check if model_path exists
+    // use std::filesystem to check if model_path(string parameter) exists
     namespace fs = std::filesystem;
-    // turn this into a conditional
-    if (!fs::exists(modelPath)) {
-        std::cerr << "Model file is not found at " << modelPath << '\n';
+    if (!(fs(model_path))) {
+        std:;cerr << "Model does not exist at: " << model_path << '\n';
         return false;
     }
+    
+    // clear session, can use clear on unique pointers
+    session_.clear();
 
-    // crearte onnxruntime enviornment
-
-    // clear previous session
-    session_.clear()
-
-    // create session, using env, model path, and session_options
-    // pass the environment and session options by value
-    session_ = std::make_unique<Ort::Session>(*env_, model_path.c_str(), *session_options_);
-
-    // reset metadata
-    input_names_.clear();
-    output_names_.clear();
-    input_shapes_.clear();
-    output_shapes_.clear();
-
-    // declare ort allocator, way to access standard memory management provided by ort
-    Ort::AllocatorWithDefaultOptions allocator;
-
-    // declare input and output nodes
-    size_t num_input_nodes = session_->GetInputCount();
-    size_t num_output_nodes = session_->GetOutputCount();
-
-    // populate input names and shapes
-    for (int i = 0; i < num_input_nodes; i++) {
-        // allocate each node/index and store in char pointer
-        char* name = session_->GetInputName(i, allocator);
-        input_names_.push_back(name); 
-
-        // utilize type info ort struct, type info -> tensor info -> tensorshapeandinfo
-        // need these series of steps to populate the input shape 
-        Ort::TypeInfo type_info = session_->GetTypeInfo();
-        auto tensor_info = session_->GetTensorTypeAndShapeInfo();
-        input_shapes_.push_back(tensor_info.GetShape());
-    }
-
-    // populate output names and shapes
-    for (int i = 0; i < num_output_nodes = session) {
-        // allocate each node/index and store it in a char point
-        char* name = session_->GetOutputName(i, allocator);
-        output_names_.push_back(name);
-
-        // utilize type info ort struct, 
-        // type info -> tensor info -> append to oujtpit sjapes
-        Ort::TypeInfo type_info = session_->GetTypeInfo();
-        auto tensor_info = session_->GetTensorTypeAndShapeInfo();
-        output_shapes_.push_back(tensor_info.GetShape());
-    }
+    // intilize session with constructor configurations, pointer to environment, 
+    // model_path as a c string, and pointer to session_options
+    // use std::make_unique to create and return a unique pointer to a newly allocted object
+    session_ = std::make_unique<Ort::Session>(*env, model_path.c_str(), *session_options);
 
     return true;
-
 }
 
 // before moving onto other memb functions, write down all onnx methods
 
 // Run inference on preprocessed input
 
-// find out what it means to run inference on preprocessed input
 std::vector<float> Engine::runInference(const cv::Mat& preprocessedInput) {
     // definition of runningInference: Takes the preprocessedInput and runs it through
     // the model, and return the results
@@ -107,7 +70,7 @@ std::vector<float> Engine::runInference(const cv::Mat& preprocessedInput) {
     // 1. Convert preprocessedInput to a float if needed
     // find out what begin and end does, how begin and end
     // allows me to fully convert a cv::Mat object into a float object
-    std::vector<float> inputTensorValues = (preprocessedInput.begin<float>, preprocessedInput.end<float>);
+    std::vector<float> inputTensorValues(preprocessedInput.begin<float>, preprocessedInput.end<float>);
     // 2. create an onnx tensor from the preprocessedinput (integral)
 
     // define input tensor shape as {1, 3, 640, 640}
@@ -117,7 +80,7 @@ std::vector<float> Engine::runInference(const cv::Mat& preprocessedInput) {
     // tensor data will reside on the CPU, so need to use CreateCPU api part of MemoryInfo
     // creating tensors using memoryInfo class, and createCPU memberfunction of this class, along with device
     // allocator and OrtmemTypeCPU as parameters
-    Ort::MemoryInfo memoryInfo= Ort::MemoryInfo::CreateCPU(OrtDeviceAllocator, OrtMemTypeCPU);
+    Ort::MemoryInfo memoryInfo = Ort::MemoryInfo::CreateCPU(OrtDeviceAllocator, OrtMemTypeCPU);
 
     // create variable called input tensor and initialize it with the CreateTensor Ort API function
     // initialize CreateTensor with float converted preprocessed image, data and size of the inputted tensor values,
@@ -140,45 +103,89 @@ std::vector<float> Engine::runInference(const cv::Mat& preprocessedInput) {
     // output_names_.size(): size of output nodes
     // 1: number of input tensors passing in: must be specified in onnx runtime
 
-    auto outputTensor = session_.Run(Ort::SessionOptions{nullptr}, 
+    auto outputTensors = session_.Run(Ort::SessionOptions{nullptr}, 
     input_names_.data(), &inputTensor, 1, output_names_.data(), output_names_.size());
 
     // 5. convert Ort::value to a float value
-    // 6. return result to be preprocessed
-    // use tensorToVector helper finmction, pass in outputTensor(first value) as the parameter
 
-    return tensorToVector(outputTensor[0]);
+    // convert using GetTensorMutableData from onnx class
+    float* floatArray = outputTensors[0].GetTensorMutableData();
+    // tensorInfo
+    auto tensorInfo = outputTensor[0].GetTensorTypeAndShapeInfo();
+    // num of elements in tensor, using elementcount from tensortypeandhshapeinfo
+    size_t numElements = tensorInfo.GetElementCount();
+
+    // return coverted tensor to float vector
+    return std::vector<float>(floatArray, floatArray + numElements);
 }
 
-    
+/* Don't even need likely, keep in case
+// if useful later
+*/
 // Get model input dimensions
 cv::Size Engine::getInputSize() const {
     // TODO: Return model input dimensions (usually 640x640 for YOLOv8)
     // return input size
-    return input_shapes_;
+    return input_shapes_[0];
 }
 
 // Get number of classes the model can detect
+/*
+Return the number of classes in the model
+Just return [0] and [1] of output_shapes_
+Maybe useful in future implementations
+*/
 int Engine::getNumClasses() const {
     // TODO: Calculate from output tensor shape
     // TODO: YOLOv8 output format: [1, 84, 8400] where 84 = 4(bbox) + 80(classes)
     // return number of classes
-}
-
-// private member functions
-
-bool Engine::initializeSession(const std::string& model_path) {
-    // initialize the onnx runime sessions
+    return output_shapes_[0][1];
 }
 
 void Engine::extractModelMetadata() {
-    // extract model metadata
+    // input and output metadata population
 
-    // Get the input/output names and shapes
-}
+    // reset metadata, can use clear maybe both names and shapes are vector containers
+    input_names_.clear();
+    input_shapes_.clear();
+    output_names_.clear();
+    output_names_.clear();
 
-std::vector<float> Engine::tensorToVector(const Ort::Value& tensor) {
-    // helper function to convert tensor to vector, will be used in runInference
+    // declare allocator with the default options from onnx c++ api
+    Ort::AllocatorWithDefaultOptions allocator;
 
-    // Convert Ort::value tensor to a c++ vector
+    // declare input and output nodes that we will iterate through, based on functions
+    // that will be used with session_ object and from Session class
+    size_t num_input_nodes = session_.GetInputCount();
+    size_t num_output_nodes = session_.GetOutputCount();
+
+    // populate input metadata
+    // emplace back used when the goal is to construct an object
+    // directly within a containers allocatd memory (passing allocator)
+    // within name char pointer onto input_names
+    for (int i = 0; i < num_output_nodes; i++) {
+        char* name = session_->GetInputCount(i, allocator);
+        input_names_.emplace_back(name);
+        allocator.Free(name);
+
+        // utlize type info struct
+        Ort::TypeInfo type_info = session_->GetTypeInfo();
+        auto tensor_info = type_info._GetTensorTypeAndShapeInfo();
+        input_shapes_.push_back(tensor_info.GetShape());
+    }
+    
+    // populate output metadata
+    for(int i = 0; i < num_output_nodes; i++) {
+        // name
+        // go from raw char pointer of name, to input names, then freeing allocated mem
+        char* name = session_->GetOutputCount(i, allocator);
+        output_names_.emplace_back(name);
+        allocator.Free(name);
+
+        // shape
+        // utilaize typeInfo struct again
+        Ort::TypeInfo type_info = session_->GetTypeInfo();
+        auto tensor_info = type_info.GetTensorTypeAndShapeInfo();
+        output_shapes_.push_back(tensor_info.GetShape());
+    }
 }
